788355613254a33844f726fff2e81ac2560a69dd
Vipul Gupta <vipulgupta0615@gmail.com>
2024-07-11 15:12:11 +0530

    11-07-2024

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './header/header';
+
+import Header from '../pages/header/header';
 export default function Home() {
   return (
 <div>
