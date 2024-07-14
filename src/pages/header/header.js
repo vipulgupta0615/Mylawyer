e4f92a3ce6e3000533bb13a9b57fa1799e358fb8
Vipul Gupta <vipulgupta0615@gmail.com>
2024-07-14 20:45:36 +0530

    14-07-2024

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -53,7 +53,7 @@ export default function Header() {
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-collapse" id="navbarCollapse">
                         <div class="navbar-nav mr-auto py-0">
                             <a href="index.html" class="nav-item nav-link active">Home</a>
                             <a href="about.html" class="nav-item nav-link">About</a>
